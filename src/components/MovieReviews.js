3e5ad1f13467f41548966c57715ecb2efe0ac80c
Daniel Mack <daniel.mack@powerhrg.com>
2018-10-10 16:17:15 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,24 @@
 // Code MovieReviews Here
+
+import React from 'react';
+
+const Review = ({
+  headline,
+}) => {
+  return (
+
+    <div key={headline} className="review" >
+        <a className="review-link" >
+          {headline}
+        </a>
+    </div>
+  );
+};
+
+const MovieReviews = ({ reviews }) => <div className="review-list">{reviews.map(Review)}</div>;
+
+MovieReviews.defaultProps = {
+  reviews: []
+}
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,23 @@
 // Code SearchableMovieReviewsContainer Here
+import React from 'react'
+import MovieReviews from './MovieReviews';
+
+export default class SearchableMovieReviewsContainer extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchTerm: "",
+      reviews: []
+    };
+  }
+
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+        <p>Reviews: {this.state.reviews.length}</p>
+        <MovieReviews reviews={this.state.reviews}/>
+      </div>
+    )
+  }
+}
